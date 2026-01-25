31d80df0369613ea54391d8465b7027dfe734ac9
rashi-kashyap09 <kashyaprashi119@gmail.com>
2026-01-25 17:07:07 +0530

    Create sum_digits.c

---
+++ sum_digits.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int n, sum = 0;
+
+    printf("Enter number: ");
+    scanf("%d", &n);
+
+    while (n != 0)
+    {
+        sum += n % 10;
+        n = n / 10;
+    }
+
+    printf("Sum of digits = %d", sum);
+    return 0;
+}
