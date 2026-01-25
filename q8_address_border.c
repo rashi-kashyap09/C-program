21e0b577932219fe65a2857b733d46a77e928f10
rashi-kashyap09 <kashyaprashi119@gmail.com>
2026-01-25 16:10:11 +0530

    Create q8_address_border.c

---
+++ q8_address_border.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    printf("*****************\n");
+    printf("* Rashi        *\n");
+    printf("* Dehradun     *\n");
+    printf("* Uttarakhand  *\n");
+    printf("*****************\n");
+    return 0;
+}
