eb4b11f1091f052503de3532a9f6c0137f1526b2
rashi-kashyap09 <kashyaprashi119@gmail.com>
2026-01-25 16:25:46 +0530

    Create symbolic_constant.c

---
+++ symbolic_constant.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PI 3.14
+
+int main() {
+    float r, area;
+
+    printf("Enter radius: ");
+    scanf("%f", &r);
+
+    area = PI * r * r;
+
+    printf("Area of circle = %.2f", area);
+    return 0;
+}
