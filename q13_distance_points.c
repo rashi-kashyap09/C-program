ad339e5c4cdb361f10676c50797549b7cc811c21
rashi-kashyap09 <kashyaprashi119@gmail.com>
2026-01-25 16:15:58 +0530

    Create q13_distance_points.c

---
+++ q13_distance_points.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    float x1, y1, x2, y2, d;
+    printf("Enter x1 y1: ");
+    scanf("%f %f", &x1, &y1);
+    printf("Enter x2 y2: ");
+    scanf("%f %f", &x2, &y2);
+
+    d = sqrt((x2-x1)*(x2-x1) + (y2-y1)*(y2-y1));
+    printf("Distance = %.2f\n", d);
+
+    return 0;
+}
