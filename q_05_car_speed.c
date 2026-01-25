c6cddd5d13139ae628f96d7710c566fd9439d973
rashi-kashyap09 <kashyaprashi119@gmail.com>
2026-01-25 16:07:33 +0530

    Create q_05_car_speed.c

---
+++ q_05_car_speed.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    float distance, time, speed;
+
+    printf("Enter distance: ");
+    scanf("%f", &distance);
+    printf("Enter time: ");
+    scanf("%f", &time);
+
+    speed = distance / time;
+    printf("Speed = %.2f\n", speed);
+
+    return 0;
+}
