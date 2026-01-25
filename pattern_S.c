10f3abdc82eb0f0fb200f71e5cb672f602b1f95b
rashi-kashyap09 <kashyaprashi119@gmail.com>
2026-01-25 17:09:58 +0530

    Create pattern_S.c

---
+++ pattern_S.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    for (i = 1; i <= 5; i++)
+        printf("* * * *\n");
+
+    printf("*\n");
+    printf("*\n");
+
+    for (i = 1; i <= 5; i++)
+        printf("* * * *\n");
+
+    return 0;
+}
