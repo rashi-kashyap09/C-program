c89debd9294d181987c83ebfe86418edd0f12402
rashi-kashyap09 <kashyaprashi119@gmail.com>
2026-01-25 17:11:53 +0530

    Create prime_range.c

---
+++ prime_range.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, j, flag;
+
+    for (i = 2; i <= 100; i++)
+    {
+        flag = 0;
+
+        for (j = 2; j <= i / 2; j++)
+        {
+            if (i % j == 0)
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        if (flag == 0)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
